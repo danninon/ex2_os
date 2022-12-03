d9029d609f14d13814b486029ac1a4f37b324b82
Dan <danninon@gmail.com>
2022-12-03 18:52:53 +0200

    no compilation errors, before tests

--- q1_ex2.c
+++ q1_ex2.c
@@ -8,8 +8,12 @@
 #include <time.h>
 #include <fcntl.h>
 #include <bits/fcntl-linux.h>
-int open_file(char* name);
-void redirect(int fd_close, char* path_open);
+#include <sys/stat.h>
+
+int open_file(char *name);
+
+void redirect(int fd_close, char *path_open);
+
 void exit_error_msg();
 
 void safe_pipe(int *pipefd);
@@ -22,6 +26,8 @@ void case_hello_who();
 #define K_HELLO "hello"
 #define MAX_NAME_LEN 10
 #define FILE_MAX_STUDENTS 100
+#define MAX_ARGS 100
+#define MAX_ARG_LEN 1000
 #define MAX_FILE_NAME_LEN 1000
 #define DYNAMIC_ARG_SIZE MAX_FILE_NAME_LEN*sizeof(char) + FILE_MAX_STUDENTS * MAX_NAME_LEN
 //case1:
@@ -31,92 +37,103 @@ void case_hello_who();
 //argv[3] = hello_who
 
 
-typedef char* Name;
-typedef char* Path;
 
-int main(int argc, char* argv[]){
+void case_hello(char **new_argv, char *inp_path);
 
+void case_hello_who(char **new_argv, char *names_file);
 
+void clean_standard_buffer();
 
-
-
-    char* inp_path = argv[1];
+int main(int argc, char *argv[]) {
+    clean_standard_buffer();
 
     int pid = fork();
-
-    char** new_argv;//will be new argv for exec
-
-    if (strcmp(inp_path, K_HELLO) == 0)
-    { //argv == {"K_HELLO"}
-        argv = &inp_path;
-    } //only the name is needed for the at argv
-    else if (strcmp(inp_path, K_HELLO_WHO) == 0) //only operate on these two
-    {
-        case_hello_who();
-
-    }//need to read txt file to and each line insert to argv (use pipe)
-    else
+    if (pid == -1) {
         exit_error_msg();
-    //case redirect ( 3 parameters )
-    if (argc != 4 && argc != 2)
-        exit_error_msg();
+    }
 
-    if (argc == 4) { //add redir
-        if (pid == 0) {
-            //case 3 parameters
-            if (strcmp(inp_path, "1>") != 0 && strcmp(argv[1], "2>&1") && strcmp(argv[1], "1>") != 0)
-                exit_error_msg();
-            if (strcmp(inp_path, "1>") == 0 || strcmp(argv[1], "2>&1"))
-                redirect(STDERR_FILENO, inp_path);
-            if (strcmp(inp_path, "2>") == 0 || strcmp(argv[1], "2>&1"))
-                redirect(STDOUT_FILENO, inp_path); //try with pipes
+    char **new_argv = NULL;
+
+    char *inp_path = argv[1];
+
+    if (pid == 0) { //son goes brrrr
+        if (strcmp(inp_path, K_HELLO) == 0) { //argv == {"K_HELLO"}
+            case_hello(new_argv, inp_path);
+        } //only the name is needed for the at argv
+        else if (strcmp(inp_path, K_HELLO_WHO) == 0) //only operate on these two
+        {
+            case_hello_who(new_argv, inp_path);
+        }//need to read txt file to and each line insert to argv (use pipe)
+        else //not valid program(assignment requested)
+            exit_error_msg();
+
+        //redirect if needed
+        //case invalid input
+        if (argc != 4 && argc != 2)
+            exit_error_msg();
+            //case redirect ( 3 parameters )
+        else if (argc == 4) { //add redir
+            if (pid == 0) {
+                //case 3 parameters
+                if (strcmp(inp_path, "1>") != 0 && strcmp(argv[1], "2>&1") && strcmp(argv[1], "1>") != 0)
+                    exit_error_msg();
+                if (strcmp(inp_path, "1>") == 0 || strcmp(argv[1], "2>&1"))
+                    redirect(STDERR_FILENO, inp_path);
+                if (strcmp(inp_path, "2>") == 0 || strcmp(argv[1], "2>&1"))
+                    redirect(STDOUT_FILENO, inp_path); //try with pipes
+            }
+
         }
+        //runs program
+        execve(new_argv[0], new_argv, NULL); //will be freed since no one is pointering it
+    } else {
+        wait(NULL); //dad goes zZzZz
+
 
     }
-    execve(new_argv[0], new_argv, NULL);
     return 0;
 }
 
-void case_hello_who() {
-
-    int num_of_sl
-    //read stud_file - import from old project
-
-    //allocate dynamic array
-
-    //allocates static array
-    char* new_argv[DYNAMIC_ARG_SIZE]; //argv[0] + argv[i] 0<i<student names in file
-    new_argv[0] = K_HELLO;
-
-    for
-    //read students into array
-
-
-    exec_stud_prog(inp_path, new_argv); //reads inp_path to new_argv
-
-
-    //free dynamic array
+void clean_standard_buffer() {
+    setbuf(stderr, NULL);
+    setbuf(stderr, NULL);
+    setbuf(stdin, NULL);
 }
 
-void exec_stud_prog(const char *inp_path, char **new_argv) {
-    char*  line = NULL;
-    ssize_t nread;
-    size_t len = 0;
 
-    FILE* stream = fopen(inp_path, "r");
-    if (stream == NULL) {
+void case_hello(char **argv, char *prog_name) {
+    argv = calloc(2, sizeof(MAX_ARG_LEN)); //could be made smaller, not bothering.
+    strcpy(argv[0],K_HELLO_WHO);
+}
+
+FILE *open_read(char *file_path) {
+    FILE *fp = fopen(file_path, "r");
+    if (fp == NULL) {
         exit_error_msg();
     }
+    return fp;
+}
 
-    //read file lines to argv untill eof
-    new_argv[0] = K_HELLO_WHO;
-    nread = getline(&line, &len, stream) != -1;
-    for (int i = 0 ; nread; ++i) {
-       strcpy(new_argv[i],line);
-        nread = getline(&line, &len, stream) != -1;
+void case_hello_who(char **new_argv, char *file_path) {
+    //allocates
+    new_argv = malloc(sizeof(char));
+    for (int i = 0; i < FILE_MAX_STUDENTS; i++) {
+        new_argv[i] = malloc(MAX_NAME_LEN);
     }
-    free(line);
-    //i holds size +1
+    strcpy(new_argv[0],K_HELLO_WHO);
+    char line_buf[MAX_NAME_LEN + 1];
+
+    /* Open the file for reading */
+    FILE *in_fp = open_read(file_path);
+    int i = 1;
+    //clean buffer pls
+    while (fgets(line_buf, MAX_NAME_LEN, in_fp) != NULL) {
+        strcpy(new_argv[i++], line_buf);
+        // ((ch = getc(in_fp)) != EOF) && (ch != '\n')
+        // fseek(in_fp, -1, SEEK_CUR); //returns effect of getc (removes from line_buf)
+        //get next student name
+    }
+    fclose(in_fp);
 }
 
 void safe_pipe(int *pipefd) {
@@ -126,28 +143,19 @@ void safe_pipe(int *pipefd) {
     }
 }
 
-void redirect(int fd_close, char* path_open){
+void redirect(int fd_close, char *path_open) {
     close(fd_close);
     open_file(path_open);
 }
-void todo3() {
 
-}
-
-void todo2() {
-
-}
-
-void todo1() {
-
-}
-void exit_error_msg(){
+void exit_error_msg() {
     perror("Exec failed: ");
-    fprintf(stderr, "Exec Failed, going to exit.\n");}
+    fprintf(stderr, "Exec Failed, going to exit.\n");
 }
 
-int open_file(char* name)
-{
+
+
+int open_file(char *name) {
     int fd;
     fd = open(name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if (fd < 0)  // open failed
@@ -155,6 +163,6 @@ int open_file(char* name)
         fprintf(stderr, "ERROR: open \"%s\" failed (%d). Exiting\n", name, fd);
         exit(2);
     }
-    fprintf(stderr, "opened file %s, file descriptor is: %d\n",name, fd);
-    return(fd);
+    fprintf(stderr, "opened file %s, file descriptor is: %d\n", name, fd);
+    return (fd);
 }
